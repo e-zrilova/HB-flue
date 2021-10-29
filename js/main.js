e92f92e0f4bd8943c9f66cde06e0215202aea63f
Elizaveta <karnizmrkiz@mail.ru>
2021-10-29 14:27:54 +0300

    fix: b => p,  border span => before

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-const swiper = new Swiper('.best-seller__slider', {
+const bestSellerSwiper = new Swiper('.best-seller__slider', {
   direction: 'horizontal',
   slidesPerView: 4,
   spaceBetween: 30,
   loop: true,
 
-
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
   navigation: {
     nextEl: '.best-seller__btn--next',
     prevEl: '.best-seller__btn--prev',
+  },
+
+
+});
+
+const workSwiper = new Swiper('.work__slider', {
+  direction: 'horizontal',
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  navigation: {
+    nextEl: '.work__slider-btn--next',
+    prevEl: '.work__slider-btn--prev',
   },
 
 
