a9db4b56bdcde122388ae30c3e89beded67eb2e9
Elizaveta <karnizmrkiz@mail.ru>
2021-11-03 23:42:47 +0300

    fix: slide div => ul>li

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const bestSellerSwiper = new Swiper('.best-seller__slider', {
 
   keyboard: {
     enabled: true,
-    onlyInViewport: false,
+    onlyInViewport: true,
   },
   navigation: {
     nextEl: '.best-seller__btn--next',
@@ -25,7 +25,7 @@ const workSwiper = new Swiper('.work__slider', {
 
   keyboard: {
     enabled: true,
-    onlyInViewport: false,
+    onlyInViewport: true,
   },
   navigation: {
     nextEl: '.work__slider-btn--next',
